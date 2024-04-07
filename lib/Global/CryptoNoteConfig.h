ae1b34d542ac3f3bf4e3a6ec6a37ac0dfddb7d82
dandzu7 <166304612+dandzu7@users.noreply.github.com>
2024-04-07 22:10:42 +0700

    Update CryptoNoteConfig.h

--- lib/Global/CryptoNoteConfig.h
+++ lib/Global/CryptoNoteConfig.h
@@ -237,8 +237,7 @@ const std::string GOVERNANCE_WALLET_ADDRESS                  = "QWC1W9dWEf955q71
 const std::string GOVERNANCE_VIEW_SECRET_KEY                 = "d6c03cec7de78fe30895bf19edeb5c24529c3d2e40d00b300fa2336c6fdafe0d";
 
 const char *const SEED_NODES[] = {
-    "103.210.69.46:8196",         //E
-    "95.216.221.239:8196"           //A
+    "103.210.69.46:8196"         //E
 };
 
 const char *const BANNED_NODES[] = {
